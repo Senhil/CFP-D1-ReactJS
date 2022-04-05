d934f75abf72104a6262f9c2d1b6fe530389b147
SK <senthilptj@gmail.com>
2022-04-05 20:09:27 +0530

    UC4 - Heading to URL by clicking logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
+import React from 'react';
 import logo from './BridgeLabzLogo.svg';
 import './App.css';
 
-function App() {
-  var name = 'Senthil'
-
-
-  
-
-  return (
-    <div>
-    <p>Hello from BridgeLabz</p>
-    <h1>Hello {name}, from Bridgelabz</h1>
-
-    
-    <img src = {logo} alt ="Just Logo" />
-    </div>
-  );
+class App extends React.Component{
+  url= "https://www.bridgelabz.com/"
+  constructor(){
+    super()
+    this.state = {
+      title: 'Welcome, click the logo' 
+    }
+  }
+  onClick=($event) =>{
+    console.log(" save button is clicked:", $event );
+    window.open(this.url, "_blank")
+  }
+  render(){
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <img src={logo} onClick={this.onClick}
+        alt="The Bridglabz logo : a Bridge to Employment through  lab works " />
+      </div>
+    );
+  }
 }
-
 export default App;

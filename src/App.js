0be74e208f9bd155f7dfef9f3de71a558ed3a143
SK <senthilptj@gmail.com>
2022-04-05 19:47:51 +0530

    UC3 - Adding logo by Property binding

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
+import logo from './BridgeLabz New Logo.svg';
 import './App.css';
 
 function App() {
   var name = 'Senthil'
 
-  
 
   
 
   return (
     <div>
-    {/* <p>Hello from BridgeLabz </p> */}
-    <h1> Hello {name}, from Bridgelabz</h1>
-
+    <p>Hello from BridgeLabz</p>
+    <h1>Hello {name}, from Bridgelabz</h1>
+    
+    <img src = {logo} alt ="Just Logo" />
     </div>
   );
 }

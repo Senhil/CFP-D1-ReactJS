4a9e3a0341e2ac41e4c2ddc61421ed41ad1a3e23
SK <senthilptj@gmail.com>
2022-04-06 23:04:37 +0530

    UC7 - Hello with complete texts

---
+++ src/Components/Blogs.js
@@ -0,0 +1,5 @@
+const About = () => {
+    return <a href= "https://www.ambitionbox.com/reviews/bridgelabz-solutions-reviews/ "><h1 className="Homme">About</h1></a>;
+  };
+  
+  export default About; 
